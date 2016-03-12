68a49a8f6b564b2b8d237668a9e05835b48f38fe
Qianzf <mailbox1024@163.com>
2016-03-12 17:08:02 +0800

    update:2016/03/12 周六17:08:02.15,Author:Qianzf

---
+++ js/qzf_function.js
@@ -0,0 +1,29 @@
+
+//jQuery图片旋转
+$(document).ready(function () {
+		var value = 0
+		$('#photo').rotate({ 
+		   bind: 
+		     { 
+		        mouseover: function(){
+		            value +=90;
+		            $(this).rotate({ animateTo:value})
+		        }
+		     } 
+		});
+		rotation();	
+	});	
+
+
+/*扩展和收缩菜单
+*/
+function resize(obj){
+	var temp = document.getElementById(obj.id).style.width;
+	//alert(temp);
+	if(temp!='100%'){
+		
+		document.getElementById(obj.id).style.width="100%";
+	}
+	else
+		document.getElementById(obj.id).style.width="85%";
+}
